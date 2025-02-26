9118f0967515bf7e582e3bc68e1dbe347c26d67e
CDOM201 <carlos@carlosdaniel.info>
2025-02-26 11:33:50 -0700

    Add PicturesComponent with routing; update DvipComponent title and navigation structure for a clearer user experience

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,15 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
 import { DvipComponent } from './dvip/dvip.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PicturesComponent } from './pictures/pictures.component';
 
 export const routes: Routes = [
     // { path: '', component: LandingPageComponent, title: "Burst Van Pictures" },
     { path: '', redirectTo: 'login/driver', pathMatch: 'full' },
     { path: 'login/:type', component: LoginComponent, title: "Login" },
-    { path: 'dvip/pictures', component: DvipComponent, title: "Pictures" },
+    { path: 'dvip/pictures', component: DvipComponent, title: "DVIP" },
     { path: 'dashboard', component: DashboardComponent, title: "Dashboard" },
+    { path: 'pictures', component: PicturesComponent, title: "Pictures" },
+    { path: 'logout', redirectTo: 'login/driver' },
     { path: '**', redirectTo: 'login/driver' }
 ];
---
+++ src/app/pictures/pictures.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-pictures',
+  imports: [NavbarComponent],
+  templateUrl: './pictures.component.html',
+  styleUrl: './pictures.component.css'
+})
+export class PicturesComponent {
+
+}
